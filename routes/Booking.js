51d4f708e48e9b55f13ed4d50af558395853f8a2
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-06-19 19:58:42 +0530

    Delete routes directory

--- routes/Booking.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookMajor } = require('../controller/bookingController');
-
-// Removed `auth` middleware
-router.post('/api/booking', bookMajor);
-
-module.exports = router;
