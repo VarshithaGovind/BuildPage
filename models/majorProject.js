0e8c13c74361e75045f6264d68aa9cb118faf8f9
VarshithaGovind <167661585+VarshithaGovind@users.noreply.github.com>
2025-06-19 19:58:24 +0530

    Delete models directory

--- models/majorProject.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const MajorSchema = new mongoose.Schema({ 
-    title: { type: String, required: true },
-    description: { type: String },
-    isFree: { type: Boolean, default: false }
-})
-
-module.exports = mongoose.model('Major', MajorSchema);
